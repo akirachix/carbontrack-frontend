eeb0ca2fdc2ddd243529ab4d22b354b106e24278
Bonnette <bonnetteumu@gmail.com>
2025-09-16 11:18:01 +0300

    added new changes

--- carbontrack/src/app/page.tsx
+++ carbontrack/src/app/page.tsx
@@ -1,9 +1,10 @@
 "use client"
+import Sidebar from "./sharedComponents/KtdaSideBar";
 
 export default function Home() {
   return (
     <div>
-     
+     <Sidebar/>
     </div>
   );
 }
--- carbontrack/src/app/sharedComponents/KtdaSideBar/index.tsx
+++ carbontrack/src/app/sharedComponents/KtdaSideBar/index.tsx
@@ -10,12 +10,12 @@ import { CiLogout } from "react-icons/ci";
 import Link from "next/link";
 
 const Sidebar = () => {
-  const pathname = usePathname(); 
+  const pathname = usePathname();
   const navItems = [
     { href: "/", Icon: RxDashboard, label: "Dashboard" },
     { href: "/emissionheatmap", Icon: FiCloud, label: "Emissions" },
-    { href: "/factory", Icon: TbBuildingFactory, label: "Factories" }, 
-    { href: "/compliance", Icon: GoVerified, label: "Compliance" }, 
+    { href: "/factory", Icon: TbBuildingFactory, label: "Factories" },
+    { href: "/compliance", Icon: GoVerified, label: "Compliance" },
   ];
   return (
     <div className="flex h-screen bg-black">
@@ -29,20 +29,20 @@ const Sidebar = () => {
           />
         </div>
         <nav className="flex flex-col flex-grow px-8 pt-7">
-          {navItems.map(({ href, Icon, label }) => {
+
+          {navItems.map((item,index) => {
+            const { href, label, Icon } = item;  
             const isActive = pathname === href;
             return (
               <Link
-                key={label}
+                key={index}
                 href={href}
-                className={`flex items-center space-x-3 mb-5 group p-2 rounded-md transition-all ${
-                  isActive ? "bg-[#F79B72] mr-5" : "hover:bg-[#F79B72] hover:mr-5"
-                }`}
+                className={`flex items-center space-x-3 mb-5 group p-2 rounded-md transition-all ${isActive ? "bg-[#F79B72] mr-5" : "hover:bg-[#F79B72] hover:mr-5"
+                  }`}
               >
                 <Icon
-                  className={`h-8 w-8 ${
-                    isActive ? "text-[#2A4759]" : "text-[#F79B72] group-hover:text-[#2A4759]"
-                  }`}
+                  className={`h-8 w-8 ${isActive ? "text-[#2A4759]" : "text-[#F79B72] group-hover:text-[#2A4759]"
+                    }`}
                 />
                 <h1
                   className={`text-[22px] ${isActive ? "text-white" : "text-white"}`}
@@ -67,3 +67,5 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
+
+
