aaba03376bc90888cc3eef3c0a1c4a9bbc5f59fd
berissamuyizere <139049950+berissamuyizere@users.noreply.github.com>
2025-09-21 20:40:44 +0300

    modified tests

--- carbontrack/src/app/hooks/useFetchCompliance.test.tsx
+++ carbontrack/src/app/hooks/useFetchCompliance.test.tsx
@@ -63,44 +63,4 @@ describe("useFetchCompliance Hook", () => {
     expect(result.current.error).toBeNull();
   });
 
-  it("should post compliance successfully", async () => {
-    (fetchCompliance as jest.Mock).mockResolvedValueOnce({ results: [] }); 
-    (fetchCompliance as jest.Mock).mockResolvedValueOnce({
-      results: [
-        {
-          compliance_id: 2,
-          compliance_target: "30",
-          compliance_status: "non-compliant",
-          created_at: "2025-01-03",
-          updated_at: "2025-01-04",
-          factory: 2,
-        },
-      ],
-    });
-    (fetch as jest.Mock).mockResolvedValueOnce({
-      ok: true,
-      json: async () => ({}),
-    });
-    const { result } = renderHook(() => useFetchCompliance());
-
-    await act(async () => {
-      await Promise.resolve();
-    });
-
-    expect(result.current.compliance).toHaveLength(1);
-    expect(result.current.compliance[0].compliance_target).toBe("30");
-  });
-
-  it("should throw error on post compliance failure", async () => {
-    (fetchCompliance as jest.Mock).mockResolvedValueOnce({ results: [] });
-    (fetch as jest.Mock).mockResolvedValueOnce({
-      ok: false,
-      text: async () => "Failed to save compliance target",
-    });
-    const { result } = renderHook(() => useFetchCompliance());
-
-    await act(async () => {
-      await Promise.resolve();
-    });
-  });
 });
