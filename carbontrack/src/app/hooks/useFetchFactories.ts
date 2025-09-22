36db2a8d803f7d312e505a4251206064c6621869
emebetgirmay <girmaayemebet@gmail.com>
2025-09-22 18:45:45 +0300

    Resolved conflicts

--- carbontrack/src/app/hooks/useFetchFactories.ts
+++ carbontrack/src/app/hooks/useFetchFactories.ts
@@ -1,39 +1,3 @@
-// <<<<<<< HEAD
-// "use client";
-// import { useEffect, useState } from "react";
-// import { fetchFactories } from "../utils/fetchFactories";
-
-// interface Factory {
-//   factory_id: string;
-//   factory_name: string;
-// }
-
-// export function useFetchFactories() {
-//   const [factories, setFactories] = useState<Factory[]>([]);
-//   const [loadingFactories, setLoadingFactories] = useState(false);
-//   const [factoryError, setFactoryError] = useState<string | null>(null);
-
-//   useEffect(() => {
-//     const loadFactories = async () => {
-//       setLoadingFactories(true);
-//       setFactoryError(null);
-//       try {
-//         const data = await fetchFactories();
-//         setFactories(data);
-//       } catch (error) {
-        
-//           setFactoryError((error as Error).message);
-        
-//       } finally {
-//         setLoadingFactories(false);
-//       }
-//     };
-//       loadFactories();
-//   }, []);
-
-//   return { factories, loadingFactories, factoryError };
-// }
-
 import { useEffect, useState } from "react";
 import { fetchFactories } from "../utils/fetchFactories";
 
