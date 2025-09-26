ce950b5e67732e719226734f610c2c11f7c50e52
BerissaMuyizere <muyizereberissa@gmail.com>
2025-09-26 12:11:02 +0300

    updated mqtt file

--- carbontrack/src/app/hivemq/mqtt_client.js
+++ carbontrack/src/app/hivemq/mqtt_client.js
@@ -1,22 +1,27 @@
 'use client';
 import { useEffect } from 'react';
 import mqtt from 'mqtt';
+
+const base_Url = process.env.BASE_URL
 const MqttSubscriber = () => {
   useEffect(() => {
-const broker = 'wss://57652ef3c2f34337b4fe5619db6f16b9.s1.eu.hivemq.cloud:8884/mqtt';
+    const broker = process.env.NEXT_PUBLIC_MQTT_BROKER;
     const options = {
-      username: 'CarbonTrack',
-      password: '@Carbontrack2025',}
+      username: process.env.NEXT_PUBLIC_MQTT_USERNAME,
+      password: process.env.NEXT_PUBLIC_MQTT_PASSWORD,
+    };
     const client = mqtt.connect(broker, options);
+
     client.on('connect', () => {
       client.subscribe('esp32/hello', (err) => {
         if (err) {
-          return Error('Subscription error:', err);
+          console.error('Subscription error:', err);
         } else {
-          return 'Subscribed to topic esp32/hello';
+          console.log('Subscribed to topic esp32/hello');
         }
       });
     });
+
     client.on('message', (topic, message) => {
       try {
         const mqttData = JSON.parse(message.toString());
@@ -24,7 +29,7 @@ const broker = 'wss://57652ef3c2f34337b4fe5619db6f16b9.s1.eu.hivemq.cloud:8884/m
           device_id: mqttData.device_id,
           emission_rate: mqttData.co2_emission_kgs,
         };
-        fetch('https://carbon-track-680e7cff8d27.herokuapp.com/api/emissions/', {
+        fetch(base_Url, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -33,26 +38,31 @@ const broker = 'wss://57652ef3c2f34337b4fe5619db6f16b9.s1.eu.hivemq.cloud:8884/m
         })
           .then(res => res.json())
           .then(response => {
-            return 'Backend API response:', response;
+            console.log('Backend API response:', response);
           })
           .catch(error => {
-            return Error('Error posting to backend:', error);
+            console.error('Error posting to backend:', error);
           });
       } catch (err) {
-        return Error('Failed to parse MQTT message JSON', err);
+        console.error('Failed to parse MQTT message JSON', err);
       }
     });
+
     client.on('error', (err) => {
       console.error('MQTT Client Error:', err);
       client.end();
     });
+
     client.on('reconnect', () => {
-      return 'MQTT reconnecting...';
+      console.log('MQTT reconnecting...');
     });
+
     return () => {
       client.end();
     };
   }, []);
+
   return null;
 };
+
 export default MqttSubscriber;
