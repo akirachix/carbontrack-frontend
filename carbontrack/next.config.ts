b332746df2658bbac9c7db53e87f183601eb754e
BerissaMuyizere <muyizereberissa@gmail.com>
2025-09-26 20:56:12 +0300

    Did responsiveness

--- carbontrack/next.config.ts
+++ carbontrack/next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   turbopack: {
-            root: '/home/student/carbontrack-frontend/carbontrack', 
-          },
   images: {
     remotePatterns: [
       {
